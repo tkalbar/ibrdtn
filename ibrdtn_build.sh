de67fc0e02a579e88791c6c7efbbf5c915a0c405
tkalbar <klbrczyk@gmail.com>
2014-04-22 12:26:29 -0500

    added build script

---
+++ ibrdtn_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+
+VERSION=0.12.0
+MODULES="ibrcommon ibrdtn ibrdtnd ibrdtn-tools"
+
+# download, extract and compile all archives
+for MOD in ${MODULES}; do
+
+        cd ${MOD}-${VERSION}
+        ./configure ${CONFOPTS} $@
+
+        if [ $? -gt 0 ]; then
+                echo "error while configure ${MOD}"
+                exit 1
+        fi
+
+        make
+
+        if [ $? -gt 0 ]; then
+                echo "error while compiling ${MOD}"
+                exit 1
+        fi
+
+        sudo make install
+
+        cd ..
+
+done
