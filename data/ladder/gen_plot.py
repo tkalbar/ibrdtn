2c24c9ad2babbb77a9b4269ac5eb9533d2f1394f
tkalbar <klbrczyk@gmail.com>
2014-05-17 19:59:05 -0500

    added a plot for ladder 1, it looks broken though

---
+++ data/ladder/gen_plot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import glob
+import os
+#os.chdir("/")
+
+newFile = open("pgy.txt", 'w+')
+newFile.write("x y\n")
+for file in glob.glob("*.dat"):
+    #newFile.write("x y")
+    fname = os.path.basename(file)
+    #label = fname[len(fname)-5]
+    fTokens = fname.strip().split('-')
+    last = fTokens[len(fTokens)-1]
+    fTokens = last.strip().split('.')
+    label = fTokens[0]
+    
+    oldFile = open(file)
+    list = []
+    for line in oldFile:
+    	tokens = line.strip().split()
+    	del tokens[0]
+    	#tokens.append(label)
+    	list.append(tuple(tokens))
+    #print list
+    if list:
+        #newFile = open("pgy_"+label+".txt", 'w+')
+        #newFile.write("x y\n")
+        newFile.write('\n'.join('%s %s' % x for x in list))
+        newFile.write("\n")
+        #newFile.close()
+    oldFile.close()
+newFile.close()
